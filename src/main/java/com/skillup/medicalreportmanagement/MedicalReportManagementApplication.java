3a26f6006a8fe8d4274912ae7f7b7f3217cf432f
kushan <kushan.se@creativesoftware.com>
2023-03-02 15:49:40 +0530

    Initial commit

---
+++ src/main/java/com/skillup/medicalreportmanagement/MedicalReportManagementApplication.java
@@ -0,0 +1,13 @@
+package com.skillup.medicalreportmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalReportManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalReportManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillup/medicalreportmanagement/MedicalReportManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillup.medicalreportmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalReportManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
