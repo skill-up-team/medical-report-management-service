5c44825a0ecb8ced4900cf6898774925ba685426
kushan <kushan.se@creativesoftware.com>
2023-03-09 15:14:36 +0530

    MapperClass updated and used as a generic mapper

--- src/main/java/com/skillup/medicalreportmanagement/mapper/EntityDtoMapper.java
+++ src/main/java/com/skillup/medicalreportmanagement/mapper/EntityDtoMapper.java
@@ -1,12 +1,14 @@
 package com.skillup.medicalreportmanagement.mapper;
 
 import com.skillup.medicalreportmanagement.dto.DiagnosisDto;
+import com.skillup.medicalreportmanagement.dto.LabReportDto;
 import com.skillup.medicalreportmanagement.entity.Diagnosis;
+import com.skillup.medicalreportmanagement.entity.LabReport;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface DiagnosisMapper {
+public interface EntityDtoMapper {
 
     @Mapping(target="diagnosisId", source="entity.id")
     @Mapping(target="diagnosisDescription", source="entity.description")
@@ -15,5 +17,13 @@ public interface DiagnosisMapper {
     @Mapping(target="id", source="dto.diagnosisId")
     @Mapping(target="description", source="dto.diagnosisDescription")
     Diagnosis diagnosisDTOtoDiagnosis(DiagnosisDto dto);
+
+    @Mapping(target="labReportId", source="entity.id")
+    @Mapping(target="labTestResults", source="entity.testResults")
+    LabReportDto labReportToLabReportDTO(LabReport entity);
+
+    @Mapping(target="id", source="dto.labReportId")
+    @Mapping(target="testResults", source="dto.labTestResults")
+    LabReport labReportDTOtoLabReport(LabReportDto dto);
 
 }
--- src/main/java/com/skillup/medicalreportmanagement/service/DiagnosisService.java
+++ src/main/java/com/skillup/medicalreportmanagement/service/DiagnosisService.java
@@ -2,9 +2,9 @@ package com.skillup.medicalreportmanagement.service;
 
 import com.skillup.medicalreportmanagement.dto.DiagnosisDto;
 import com.skillup.medicalreportmanagement.entity.Diagnosis;
-import com.skillup.medicalreportmanagement.exception.DiagnosisExistException;
-import com.skillup.medicalreportmanagement.exception.DiagnosisNotFoundException;
-import com.skillup.medicalreportmanagement.mapper.DiagnosisMapper;
+import com.skillup.medicalreportmanagement.exception.EntryExistException;
+import com.skillup.medicalreportmanagement.exception.EntryNotFoundException;
+import com.skillup.medicalreportmanagement.mapper.EntityDtoMapper;
 import com.skillup.medicalreportmanagement.repository.DiagnosisRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,36 +18,36 @@ public class DiagnosisService {
     private DiagnosisRepository diagnosisRepository;
 
     @Autowired
-    private DiagnosisMapper diagnosisMapper;
+    private EntityDtoMapper entityDtoMapper;
 
     public DiagnosisDto createDiagnosis(DiagnosisDto diagnosisDto) {
         Diagnosis diagnosisExist = diagnosisRepository.findByDoctorAppointmentId(diagnosisDto.getDoctorAppointmentId());
         if(diagnosisExist != null) {
-            throw new DiagnosisExistException("Diagnosis record already exist for the given appointment id: " + diagnosisDto.getDoctorAppointmentId());
+            throw new EntryExistException("Diagnosis record already exist for the given appointment id: " + diagnosisDto.getDoctorAppointmentId());
         }
 
-        Diagnosis diagnosis = diagnosisMapper.diagnosisDTOtoDiagnosis(diagnosisDto);
+        Diagnosis diagnosis = entityDtoMapper.diagnosisDTOtoDiagnosis(diagnosisDto);
 
-        return  diagnosisMapper.diagnosisToDiagnosisDTO(diagnosisRepository.save(diagnosis));
+        return  entityDtoMapper.diagnosisToDiagnosisDTO(diagnosisRepository.save(diagnosis));
     }
 
     public DiagnosisDto updateDiagnosis(DiagnosisDto diagnosisDto) {
         Optional<Diagnosis> diagnosisExist = diagnosisRepository.findById(diagnosisDto.getDiagnosisId());
         if(diagnosisExist.isEmpty()) {
-            throw new DiagnosisNotFoundException("Diagnosis not found for the given diagnosis id: " + diagnosisDto.getDiagnosisId());
+            throw new EntryNotFoundException("Diagnosis not found for the given diagnosis id: " + diagnosisDto.getDiagnosisId());
         }
 
-        Diagnosis diagnosis = diagnosisMapper.diagnosisDTOtoDiagnosis(diagnosisDto);
+        Diagnosis diagnosis = entityDtoMapper.diagnosisDTOtoDiagnosis(diagnosisDto);
 
-        return  diagnosisMapper.diagnosisToDiagnosisDTO(diagnosisRepository.save(diagnosis));
+        return  entityDtoMapper.diagnosisToDiagnosisDTO(diagnosisRepository.save(diagnosis));
     }
 
     public DiagnosisDto getDiagnosisByAppointmentId(Integer doctorAppointmentId) {
         Diagnosis diagnosisExist = diagnosisRepository.findByDoctorAppointmentId(doctorAppointmentId);
         if(diagnosisExist == null) {
-            throw new DiagnosisNotFoundException("Diagnosis not found for the given appointment id: " + doctorAppointmentId);
+            throw new EntryNotFoundException("Diagnosis not found for the given appointment id: " + doctorAppointmentId);
         }
 
-        return diagnosisMapper.diagnosisToDiagnosisDTO(diagnosisExist);
+        return entityDtoMapper.diagnosisToDiagnosisDTO(diagnosisExist);
     }
 }
